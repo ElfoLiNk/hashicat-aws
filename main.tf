e988cf5cda39a5da2650204e8f32b8cca1c1ecc2
root <root@p-htajjia2qlxx-workstation.c.instruqt-prod.internal>
2020-11-25 16:49:39 +0000

    Only me

--- main.tf
+++ main.tf
@@ -38,21 +38,21 @@ resource "aws_security_group" "hashicat" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["93.36.190.68/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["93.36.190.68/32"]
   }
 
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks     = ["93.36.190.68/32"]
     prefix_list_ids = []
   }
 
