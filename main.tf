af6077b7770904e9631e0fc18e345100d64b6343
root <root@p-htajjia2qlxx-workstation.c.instruqt-prod.internal>
2020-11-25 16:35:29 +0000

    restrict ssh cidr

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["93.36.190.68/32"]
   }
 
   ingress {
